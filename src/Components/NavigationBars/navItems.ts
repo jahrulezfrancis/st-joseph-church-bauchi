424c16b553e8d0dbe60052d16083eba55fc1bdd5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-01-08 06:49:48 +0000

    Initial commit

---
+++ src/Components/NavigationBars/navItems.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+    { id: 1, name: 'About', path: "/#about", Icon: '', },
+    { id: 2, name: 'Contact', path: "/#contact", Icon: '', },
+    { id: 3, name: 'Blog', path: "/blog", Icon: '', },
+]
