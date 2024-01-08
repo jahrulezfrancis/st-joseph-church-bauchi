51c3ecbc289890c8d73ba36026407851ee14059e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-01-08 06:53:51 +0000

    Wrapped app component with core providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+const { Button } = chakraTheme.components
+import { ChakraBaseProvider, extendBaseTheme, theme as chakraTheme, } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraBaseProvider>
   </React.StrictMode>,
 )
