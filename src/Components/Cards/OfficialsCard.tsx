0928984e95a45266df11f3fd980a94e96f150b1b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-03-05 21:50:50 +0000

    Initial commit

---
+++ src/Components/Cards/OfficialsCard.tsx
@@ -0,0 +1,23 @@
+import { Box, Stack, Text, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+interface OfficialCardProps {
+    personPosition: string;
+    personImage: string;
+    personName: string;
+}
+
+export default function ParishOfficialsCard(props: OfficialCardProps) {
+    return (
+        <Box>
+            <Stack padding={5} justify="space-between" align="center" bg="red" width={["auto", "auto", "320px"]}>
+                <Text>{props.personPosition}</Text>
+                <Image height={"300px"} objectFit="cover" width={"280px"} src={props.personImage} alt={props.personName} />
+                <Text>{props.personName}</Text>
+                <Link to="#">
+                    <Text>Biography</Text>
+                </Link>
+            </Stack>
+        </Box>
+    )
+}
