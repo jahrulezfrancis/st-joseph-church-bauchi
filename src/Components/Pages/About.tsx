e170a7bdbc6f92ecb2e62bd20a3390c3a8316ed4
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-15 11:20:20 +0100

    Initial commit

---
+++ src/Components/Pages/About.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function AboutPage() {
+    return (
+        //page should have about the church, biography and history and if possible; founders
+        <Box>
+            <Heading>You about page</Heading>
+        </Box>
+    )
+}
