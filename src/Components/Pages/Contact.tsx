2b5b97d39685c5d986285d473717a81fb1cbe32b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-15 11:20:30 +0100

    Initial commit

---
+++ src/Components/Pages/Contact.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function ContactPage() {
+    return (
+        //Should neccesary contact information including parish phone numbers and email address
+        <Box>
+            <Heading>Your contact Page</Heading>
+        </Box>
+    )
+}
